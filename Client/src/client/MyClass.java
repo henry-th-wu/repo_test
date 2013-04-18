967f947555875802fd2d0aa15a9241993d81130f
unknown <tsunwu@TSUNWU-LAP.st-users.us.oracle.com>
2013-04-18 14:28:52 -0700

    first commit

---
+++ Client/src/client/MyClass.java
@@ -0,0 +1,12 @@
+package client;
+
+public class MyClass {
+    public MyClass() {
+        super();
+    }
+    
+    public String sayHello(String name){
+        System.out.println("Hello:  "+ name);
+        return "Hello:  "+ name;
+    }
+}
